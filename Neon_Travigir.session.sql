f4f92030ce220df8b435bfef2659eb595de748b9
faresUnderScore999 <benalifares999@gmail.com>
2026-04-28 20:29:45 +0100

    first version of admin assistant

--- Neon_Travigir.session.sql
+++ Neon_Travigir.session.sql
@@ -1,1 +1,3 @@
-SELECT * FROM public.unified_events;
+
+update users set password = '$2y$12$SSywBPwHzc45o/AtiBr5cOkxfG9owsIr/bF2VIva/1B5ZThiRAktm' where email = 'benalifares999@gmail.com';
+SELECT * FROM users WHERE email ='benalifares999@gmail.com';
